9d0cd337ed6a7da765b0c21ed709156de6bab562
Collin Wheat <collinwheat@gmail.com>
2023-01-22 22:52:51 -0500

    Food Truck App

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,15 +1,77 @@
 package com.skilldistillery.foodtruck.app;
 
+import java.util.Scanner;
+
 import com.skilldistillery.foodtruck.entities.Foodtruck;
 
 public class FoodTruckApp {
-	//fleet of Foodtrucks
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		private Foodtruck[] fleet;
-		// reference parkinglot
-		
-			
-	}
 
+		Scanner input = new Scanner(System.in);
+
+		Foodtruck fleet[] = new Foodtruck[5]; // declaring/initializing fleet array
+
+		// Main app does not pass in ID value
+
+		System.out.println("Please input information for up to 5 Food Trucks"); // Needs to be able to quit at any time
+
+		Foodtruck userTruck = null; // initializing and declaring a foodtruck object null
+
+		int counter = 0;
+
+		for (int i = 0; i < fleet.length; i++) { // for loop that prompts the user to enter info on 5 food trucks
+			userTruck = new Foodtruck(); // needs to give the option to quit before 5 loops
+			System.out.println("Please enter name");
+			userTruck.setName(input.next());
+			if (userTruck.getName().equals("quit")) {
+				break;
+			}
+			System.out.println("Please enter food Type");
+			userTruck.setFoodType(input.next());
+			System.out.println("Please enter Rating 1-5");
+			userTruck.setRating(input.nextInt());
+			fleet[i] = userTruck;
+			counter = counter + 1;
+		}
+		String answer = null;
+		System.out.println(counter);
+		
+		for(;;) {
+			userTruck.displayMenu(); // calls display menu method in the Foodtruck class
+			answer = input.next(); // declaring and initializing String answer as the answer to menu prompt
+			switch (answer) { // switch to tell program what the user wanted to do
+			case "a":
+				for (int i = 0; i < counter; i++) {
+					System.out.println(fleet[i]);
+				}continue;
+				
+			case "b": // option that finds the average of all the ratings in the class and displays
+				double average = 0; // the average
+				int sum = 0;
+				for (int i = 0; i < counter; i++) {
+					sum = sum + fleet[i].getRating();
+				}
+				average = sum / fleet.length;
+				System.out.println("Average Foodtruck Rating: " + average);
+				continue;
+				
+			case "c":
+				int arrayNum = 0;
+				int max = 0;
+				int i = 0;
+				for (i = 0; i < counter; i++) {
+					if (fleet[i].getRating() > max) {
+						max = fleet[i].getRating();
+						max = fleet[arrayNum].getRating();
+					}
+				}
+				System.out.println("Highest FoodTruck Rating is: " + fleet[arrayNum].toString());
+				continue;
+				
+			case "d":
+				System.out.println("Goodbye"); // quit program
+		}break;
+//		input.close(); // closes the scanner
+		}
+	}
 }
--- src/com/skilldistillery/foodtruck/entities/Foodtruck.java
+++ src/com/skilldistillery/foodtruck/entities/Foodtruck.java
@@ -1,6 +1,108 @@
 package com.skilldistillery.foodtruck.entities;
 
-public class Foodtruck {
-	private static int nextTruckId;
-	private int id;
+//import com.skilldistillery.encapsulation.labs.Car;
+
+public class Foodtruck { // creating the class Foodtruck
+	private String name; // declaring the name field
+	private String foodType; // declaring the food type field
+	private int rating; // declaring the rating variable
+	private static Foodtruck fleet[]; // declaring but not initializing the fleet array
+	private static int id;
+	
+ 
+	public Foodtruck() {
+		fleet = new Foodtruck[5];
+//		for (int i = 0; i <= fleet.length; i++) {
+//			this.id = i;
+//		}
+//		--setup basic for loop (for i go through fleet) and pass in setid method
+		
+	}
+
+	private int getId() { // get id method for returning the id
+		return id;
+	}
+
+	public void setId(int id) { // set id method for setting the id
+		this.id = id;
+//		for (int i = 0; i < fleet.length; i++) {
+//			id = i;
+//		}
+		
+	}
+
+	public String getName() { // get name method for returning the name
+		return name;
+	}
+
+	public void setName(String name) { // set name method for setting the name
+		this.name = name;
+	}
+
+	public String getFoodType() { // get foodtype method for returning the food type
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) { // set foodtype method for setting food type
+		this.foodType = foodType;
+	}
+
+	public int getRating() { // get rating method for returning the rating
+		return rating;
+	}
+
+	public void setRating(int rating) { // set rating method for setting the rating
+		this.rating = rating;
+	}
+
+	public void displayMenu() { // display menu method for printing the menu
+		System.out.println("|-------------------------------------|");
+		System.out.println("|        A.List all Food Trucks       |");
+		System.out.println("|    B.See Average Food Truck Rating  |");
+		System.out.println("| C. Display Highest Rated Food Truck |");
+		System.out.println("|              D. Quit                |");
+		System.out.println("|-------------------------------------|");
+	}
+
+	public String toString() { //
+		String output = "Name: " + name + " Type: " + foodType + " Rating: " + rating + " ID: " + id;
+		return output;
+	}
+
+//	public void displayFleet() { // display fleet method for printing the fleet contents
+//		for (int i = 0; i <= 5; i++) {
+//			if (fleet[i] == null) {
+//				System.out.println(" ");
+//			}else {
+//				System.out.println(fleet[i]);
+//			}
+//		}
+//	}
+
+	public Foodtruck[] getTrucks() { //
+		return fleet;
+	}
+
+	public Foodtruck(String name, String type, int rating, int id) { //
+		this.rating = rating;
+		this.name = name;
+		this.foodType = type;
+//		this.id = 0;
+		for (int i = 0; i <= fleet.length; i++) {
+			this.id = i++;
+			
+			continue;
+		}
+	}
+//	public int highestRated() {
+//		int max = 0;
+//		for (int i = 0; i < fleet.length; i++) {
+//			if (fleet[i].getRating() > max) {
+//				max = fleet[i].getRating();
+//			}
+//			
+//		}
+//		return max;
+//	}
+
 }
